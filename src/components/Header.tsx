7a4e618551908364240466e2fcb921e2c8c8c70f
Sortjakke <erick.matheusrs1003@gmail.com>
2025-09-30 18:57:52 -0300

    feat: install Font Awesome icons and finishing the header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,19 @@
 import { useContext } from "react"
 import { ThemeContext } from "../context/ThemeContext"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons"
 
 function Header() {
   const { theme, toggleTheme } = useContext(ThemeContext)
 
   return (
-    <header className="flex justify-between items-center p-4 bg-elLight dark:bg-elDark">
+    <header className="flex justify-between items-center px-4 py-6 bg-elLight dark:bg-elDark">
       <h1 className="text-xl font-bold">Where in the world?</h1>
-      <button onClick={toggleTheme} className="text-sm">
+      <button
+        onClick={toggleTheme}
+        className="flex items-center gap-2 text-sm font-semibold"
+      >
+        <FontAwesomeIcon icon={theme === "light" ? faMoon : faSun} />
         {theme === "light" ? "Dark Mode" : "Light Mode"}
       </button>
     </header>
